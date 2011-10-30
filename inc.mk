675a3c90b365a68982911b060820a4f183a0f4d8
Peter Alfonso <peteralfonsojr@gmail.com>
2011-10-30 03:16:20 -0400

    Firmware is inherited from /hardware/broadcom

--- inc.mk
+++ inc.mk
@@ -65,9 +65,7 @@ PRODUCT_COPY_FILES += \
 
 # Firmware
 PRODUCT_COPY_FILES += \
-    device/htc/inc/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
-    device/htc/inc/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin \
-    device/htc/inc/firmware/fw_bcm4329_apsta.bin:system/etc/firmware/fw_bcm4329_apsta.bin
+    device/htc/inc/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd
 
 PRODUCT_COPY_FILES += \
     device/htc/inc/vold.fstab:system/etc/vold.fstab \
