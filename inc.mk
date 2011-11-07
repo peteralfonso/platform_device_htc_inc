58b9b99bc2fbd43358ced7896e59783152edef0e
Peter Alfonso <peteralfonsojr@gmail.com>
2011-11-07 00:26:00 -0500

    Remove more unneeded properties

--- inc.mk
+++ inc.mk
@@ -30,12 +30,6 @@ $(call inherit-product-if-exists, vendor/htc/inc/inc-vendor.mk)
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.cdma.home.operator.alpha=Verizon \
 	ro.cdma.home.operator.numeric=310012 \
-	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
-	ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
-	ro.config.vc_call_vol_steps=7 \
-	ro.cdma.otaspnumschema=SELC,1,80,99 \
-	ro.telephony.call_ring.multiple=false \
-	ro.telephony.call_ring.delay=3000 \
 	ro.media.dec.jpeg.memcap=20000000 \
 	ro.media.enc.jpeg.quality=95,85,70
 
